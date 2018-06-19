6229a32684953e8736c8ba01c89374beacba42f1
cvalladares <cvalladares4837@gmail.com>
2018-06-19 14:25:19 +0800

    Merge branch 'master' of https://github.com/Cvalladares/gula_apong_mobile
    Fixed conflict related to old gula apong controllers

--- www/js/SalesTodayController.js
+++ www/js/SalesTodayController.js
@@ -3,7 +3,19 @@ angular.module('Gula.controllers').controller('salesTodayCtrl', ['$scope', '$sta
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
   function ($scope, $stateParams) {
+    $scope.farmer = {price: 0, yield_weight: 0, date: new Date(Date.now())}
 
+    $scope.submit = function () { //the code below is executed when someone presses submit
+
+      PouchDBService.getProductionDb.post({
+        price: $scope.farmer.price,
+        yield_weight: $scope.farmer.yield_weight,
+        date: $scope.farmer.date,
+      })
+
+      console.log(PouchDBService.getProductionDb)
+
+    }
 
   }]);
 
