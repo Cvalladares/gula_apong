dc287e5b80bb8fa390f8b18d75a06dbbd22bf50c
judithnaomi <j.n.schermer@gmail.com>
2018-06-19 05:53:52 +0200

    Chris changed the FarmController and I changed the SalesController, and now I merged both improvements. Data entered in MyFarm and in MyHarvest are saved in two different PouchDatabases.!!

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -16,14 +16,23 @@ angular.module('Gula.controllers', [])
 
     }])
 
-  .controller('salesTodayCtrl', ['$scope', '$stateParams',
-// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-    function ($scope, $stateParams) {
+  .controller('salesTodayCtrl', function ($scope, $stateParams, PouchDBService) {
 
+    $scope.farmer = {price: 0, yield_weight: 0, date: new Date(Date.now())}
 
-    }])
+    $scope.submit = function () { //the code below is executed when someone presses submit
+
+      PouchDBService.getProductionDb.post({
+        price: $scope.farmer.price,
+        yield_weight: $scope.farmer.yield_weight,
+        date: $scope.farmer.date,
+      })
+
+      console.log(PouchDBService.getProductionDb)
+
+    }
+  })
+
 
   .controller('myFarmCtrl', function ($scope, $stateParams, PouchDBService) {
     // Fetch database Singleton
--- www/js/services.js
+++ www/js/services.js
@@ -3,5 +3,6 @@ angular.module("Gula.services", [])
     var databaseOne = new PouchDB("testDatabase"); //if the service is singleton then ...
     // is service singleton??? because we must hahve 1 object per controller. can not be the same for all. no!!
     // it should be one.
-    return {getDb: databaseOne};
-  });
+    var productionDb = new PouchDB("something");
+    return {getDb: databaseOne, getProductionDb: productionDb};
+});
