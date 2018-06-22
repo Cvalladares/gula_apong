35e9bc7e5870b20b040a186c806c01fd25069c53
Giorgi Kikolashvili <gkiko10@freeuni.edu.ge>
2018-06-22 15:14:46 +0800

    Fix null object database from pouchdb service

--- www/js/MyFarmController.js
+++ www/js/MyFarmController.js
@@ -1,32 +1,32 @@
 angular.module('Gula.controllers').controller('myFarmCtrl', function ($scope, $stateParams, PouchDBService) {
   // Fetch database Singleton
-  var db = PouchDBService.getDb
+  var db = PouchDBService.getFarmDb();
 
   // Initializing the scope to its previous value
-  db.get("myFarm").then(function (res) {
-    $scope.farmer = {trees: res.trees, distance: res.distance}
-  })
+  db.get("myFarm")
+    .then(function (res) {
+      $scope.farmer = {trees: res.trees, distance: res.distance}
+    })
     .catch(function () {
       $scope.farmer = {trees: 0, distance: 0}
-    })
+    });
 
   // This function is binded to the submit button
   $scope.submit = function () {
     // Update Farm information
-    db.get("myFarm").then(function (res) {
-      // Instance where the farm exists
-      console.log("Document Exists. Updating.")
-      db.put({
-        _id: "myFarm",
-        _rev: res._rev,
-        trees: $scope.farmer.trees,
-        distance: $scope.farmer.distance
-      }).then(function (res) {
-        console.log(res);
+    db.get("myFarm")
+      .then(function (res) {
+        // Instance where the farm exists
+        console.log("Document Exists. Updating.");
+        db.put({
+          _id: "myFarm",
+          _rev: res._rev,
+          trees: $scope.farmer.trees,
+          distance: $scope.farmer.distance
+        }).then(function (res) {
+          console.log(res);
+        })
       })
-
-
-    })
       .catch(function (res) {
         // Instance where the farm does not exist
         console.log("The document does not exist. Creating a new doc")
