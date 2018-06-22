deb7426df3da1a2bc2e6e589304c634e7256d12d
Giorgi Kikolashvili <gkiko10@freeuni.edu.ge>
2018-06-22 14:40:28 +0800

    Disable login button after login

--- www/js/LoginController.js
+++ www/js/LoginController.js
@@ -1,7 +1,18 @@
 angular.module('Gula.controllers').controller('loginCtrl', function ($scope, PouchDBService, localStorageService,
-                                                                     $state, $cordovaDialogs) {
+                                                                     $state, $cordovaDialogs, $ionicHistory) {
 
-  $scope.user = {id: "", couchPassword: ""};
+  $ionicHistory.nextViewOptions({
+    disableAnimate: true,
+    disableBack: true
+  });
+
+  $scope.$on('$ionicView.enter', function (e) {
+    $ionicHistory.clearHistory();
+    $ionicHistory.clearCache();
+
+    $scope.user = {id: "", couchPassword: ""};
+  });
+
 
   $scope.authUser = function () {
     localStorageService.set('user', $scope.user);
@@ -12,7 +23,7 @@ angular.module('Gula.controllers').controller('loginCtrl', function ($scope, Pou
 
       })
       .catch(function (err) {
-        $cordovaDialogs.alert('Whoops!', err)
+        $cordovaDialogs.alert('Whoops!', err.message);
       });
   }
 
