769b438f0d9ee43da284af5a934edc2cb73eb581
judithnaomi <j.n.schermer@gmail.com>
2018-06-25 12:24:39 +0200

    fixed login bug

--- www/js/LoginController.js
+++ www/js/LoginController.js
@@ -20,7 +20,8 @@ angular.module('Gula.controllers').controller('loginCtrl', function ($scope, Pou
     PouchDBService.initPouchDbs();
     PouchDBService.initSyncForUser()
       .then(function () {
-        $scope.goToInterface();
+        $state.go('dashboardProducer');
+       // $scope.goToInterface();
       })
       .catch(function (err) {
         $cordovaDialogs.alert( 'Username or Password incorrect', 'Whoops!');
@@ -28,18 +29,23 @@ angular.module('Gula.controllers').controller('loginCtrl', function ($scope, Pou
   }
 
   $scope.goToInterface = function () {
+
     var db = PouchDBService.getProfileDb();
 
     // Initializing the scope to its previous value
     db.get("myProfile")
       .then(function (res) {
         if ($scope.farmer.role == "producer") {
+          console.log("hoi producer");
           $state.go('dashboardProducer');
         } else if ($scope.farmer.role == "buyer") {
+          console.log("hoi producer");
           $state.go('dashboardBuyer');
         } else if ($scope.farmer.role == "minister") {
+          console.log("hoi minister");
           $state.go('dashboardMinister');
         } else // $scope.farmer.role == admin
+          console.log("you are in the else");
           $state.go('dashboardProducer');
 
       })
