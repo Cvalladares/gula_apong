0bbcf303df28fb7b71518f4a1e9b4da7f4e6209a
Giorgi Kikolashvili <gkiko10@freeuni.edu.ge>
2018-07-01 14:10:10 +0800

    Show html splinner when calculating area

--- www/js/AddFarmController.js
+++ www/js/AddFarmController.js
@@ -1,91 +1,94 @@
 angular.module('Gula.controllers').controller('addFarmCtrl', function ($scope, $rootScope, PouchDBService, localStorageService,
                                                                        $cordovaDialogs, $ionicHistory, $cordovaGeolocation, CalculateAreaService,
                                                                        LoadingAnimation) {
-    // You can use https://www.daftlogic.com/projects-google-maps-area-calculator-tool.htm
-    //    to test the calculations.
-    // let coordinates = [
-    //   [1.602810301968369,110.36798244493912],
-    //   [1.6071001528072157,110.37235981005142],
-    //   [1.609073481168485,110.37205940264175],
-    //   [1.60810826644756,110.36901241320083],
-    //   [1.6084514539562023,110.36813264864395],
-    //   [1.6077650788812101,110.3652787782521],
-    //   [1.6053413150518685,110.365729389366]
-    // ];
+  // You can use https://www.daftlogic.com/projects-google-maps-area-calculator-tool.htm
+  //    to test the calculations.
+  // let coordinates = [
+  //   [1.602810301968369,110.36798244493912],
+  //   [1.6071001528072157,110.37235981005142],
+  //   [1.609073481168485,110.37205940264175],
+  //   [1.60810826644756,110.36901241320083],
+  //   [1.6084514539562023,110.36813264864395],
+  //   [1.6077650788812101,110.3652787782521],
+  //   [1.6053413150518685,110.365729389366]
+  // ];
 
-  $scope.myGoBack = function() {
+  $scope.trackingInProgress = false;
+
+  $scope.myGoBack = function () {
     // $ionicHistory.goBack() does not work for some reason
     $state.go('login');
 
   };
 
 
-    var edit = false;
-    $scope.$on('$ionicView.enter', function (e) {
+  var edit = false;
+  $scope.$on('$ionicView.enter', function (e) {
 
-      $scope.user = {trees: '', CalculatedArea: ''};
-      if ($rootScope.areaId) {
-        edit = true;
-        PouchDBService.getFarmDb().get($rootScope.areaId)
-          .then(function (res) {
-            $scope.user = res;
-            $scope.$digest();
-          })
-      }
+    $scope.user = {trees: '', CalculatedArea: ''};
+    if ($rootScope.areaId) {
+      edit = true;
+      PouchDBService.getFarmDb().get($rootScope.areaId)
+        .then(function (res) {
+          $scope.user = res;
+          $scope.$digest();
+        })
+    }
+  });
+
+  $scope.$on('$ionicView.leave', function (e) {
+    delete $rootScope.areaId;
+  });
+
+  $scope.submit = function () {
+    LoadingAnimation.show();
+    var farmData = {
+      trees: $scope.user.trees,
+      coords: coords,
+      area: $scope.user.CalculatedArea,
+      date: JSON.stringify(new Date()),
+    };
+
+
+    var promise;
+    if (edit) {
+      promise = PouchDBService.getFarmDb().put($scope.user);
+    } else {
+      promise = PouchDBService.getFarmDb().post(farmData);
+    }
+
+    promise.then(function (res) {
+      $ionicHistory.goBack();
+    }).catch(function (err) {
+      $cordovaDialogs.alert('Data is not stored correctly. Try again.', 'Whoops!');
+      console.error(err);
+    }).finally(function () {
+      LoadingAnimation.hide();
+    });
+  };
+
+  var coords = [];
+  $scope.startTracking = function () {
+    coords = [];
+    var watch = $cordovaGeolocation.watchPosition({timeout: 10000, enableHighAccuracy: false});
+    watch.then(null, function (err) {
+      console.error(err)
+    }, function (position) {
+      var lat = position.coords.latitude;
+      var long = position.coords.longitude;
+      coords.push([lat, long]);
+      console.debug(lat+' '+long)
     });
 
-    $scope.$on('$ionicView.leave', function (e) {
-      delete $rootScope.areaId;
-    });
+    $scope.trackingInProgress = true;
+    $scope.watch = watch;
+  };
 
-    $scope.submit = function () {
-      LoadingAnimation.show();
-      var farmData = {
-        trees: $scope.user.trees,
-        coords: coords,
-        area: $scope.user.CalculatedArea,
-        date: JSON.stringify(new Date()),
-      };
-
-
-      var promise;
-      if (edit) {
-        promise = PouchDBService.getFarmDb().put($scope.user);
-      } else {
-        promise = PouchDBService.getFarmDb().post(farmData);
-      }
-
-      promise.then(function (res) {
-        $ionicHistory.goBack();
-      }).catch(function (err) {
-        $cordovaDialogs.alert('Data is not stored correctly. Try again.', 'Whoops!');
-        console.error(err);
-      }).finally(function () {
-        LoadingAnimation.hide();
-      });
-    };
-
-    var coords = [];
-
-    $scope.startTracking = function () {
-      var watch = $cordovaGeolocation.watchPosition({timeout: 10000, enableHighAccuracy: false});
-      watch.then(null, function (err) {
-        console.error(err)
-      }, function (position) {
-        var lat = position.coords.latitude;
-        var long = position.coords.longitude;
-        coords.push([lat, long]);
-        //console.log(lat + " " + long);
-      });
-
-      $scope.watch = watch;
-    };
-
-    $scope.stopTracking = function () {
-      console.log("hello we are in stop");
-      $scope.watch.clearWatch();
-      $scope.user.CalculatedArea =(CalculateAreaService.calculateAreaOfGPSPolygonOnEarthInSquareMeters(coords))
-    };
+  $scope.stopTracking = function () {
+    $scope.watch.clearWatch();
+    $scope.trackingInProgress = false;
+    $scope.user.CalculatedArea = (CalculateAreaService.calculateAreaOfGPSPolygonOnEarthInSquareMeters(coords))
+  };
 
 
 });
